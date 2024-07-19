e156a5b4cb552d15a95aa552d2f27f052c5b279e
omgSRT <thangckdt@gmail.com>
2024-07-19 13:27:40 +0700

    fix: wrong repo usage

--- src/main/java/com/GSU24SE43/ConstructionDrawingManagement/repository/VersionRepository.java
+++ src/main/java/com/GSU24SE43/ConstructionDrawingManagement/repository/VersionRepository.java
@@ -1,5 +1,7 @@
 package com.GSU24SE43.ConstructionDrawingManagement.repository;
 
+import com.GSU24SE43.ConstructionDrawingManagement.entity.Department;
+import com.GSU24SE43.ConstructionDrawingManagement.entity.Task;
 import com.GSU24SE43.ConstructionDrawingManagement.entity.Version;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,4 +15,12 @@ public interface VersionRepository extends JpaRepository<Version, UUID> {
     List<Version> findByDrawingIdAndStatus(UUID drawingId, String status);
 
     List<Version> findByStatus(String status);
+
+    List<Version> findByDrawingTaskDepartment(Department department);
+
+    List<Version> findByDrawingTaskDepartmentAndStatus(Department department, String status);
+
+    List<Version> findByDrawingTask(Task task);
+
+    List<Version> findByDrawingTaskAndStatus(Task task, String status);
 }
